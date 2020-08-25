520a49e40ad7514b1a7776fa827187da4f0a1f11
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2020-08-25 19:10:52 -0400

    Completed

---
+++ app/static/styles/index.js
@@ -0,0 +1,11 @@
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+// Adding animations to the sections
+$("section").addClass("wow fadeIn");
+
+// Animations Init
+new WOW().init();
+
